de28262bf46965aae6186822cca172fb204a632f
Tianyi Cui <1997cui@gmail.com>
2024-07-07 20:02:55 -0700

    Separate API call to dedicated API class

--- beancount_import/source/mercury.py
+++ beancount_import/source/mercury.py
@@ -14,13 +14,58 @@ from beancount_import.source import (AssociatedData, ImportResult, Source,
 
 from ..matching import FIXME_ACCOUNT
 from ..journal_editor import JournalEditor
+from datetime import datetime
+import collections
+import urllib.parse
 
 # Constants
-MERCURY_API_URL_TEMPLATE = 'https://api.mercury.com/api/v1/account/{account_id}/transactions?limit=500&offset='
 
 def allowed_gai_family(): 
     return socket.AF_INET
 urllib3_cn.allowed_gai_family = allowed_gai_family
+
+class MercuryAPI():
+    MERCURY_API_BASE="https://api.mercury.com/api/v1/"
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.accounts: List[str] = list()
+        self.fetch_accounts()
+
+    def _get_headers(self) -> Dict:
+        return {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+    
+    def fetch_accounts(self):
+        response = requests.get(urllib.parse.urljoin(
+            self.MERCURY_API_BASE, 'accounts'), headers=self._get_headers())
+        response.raise_for_status()
+        data = response.json()
+        accounts_data = data['accounts'] if 'accounts' in data else []
+        for account in accounts_data:
+            self.accounts.append(account['id'])
+
+    def fetch_mercury_transactions(self) -> Dict:
+        MERCURY_API_TXN = 'account/{account_id}/transactions?limit=500&offset={offset}'
+        'https://api.mercury.com/api/v1/account/{account_id}/transactions?limit=500&offset={offset}'
+        transactions: Dict[str, List] = {}
+        for account in self.accounts:
+            transactions[account] = []
+            offset = 0
+            partial = MERCURY_API_TXN.format(account_id=account, offset=offset)
+            full_url = urllib.parse.urljoin(self.MERCURY_API_BASE, partial)
+            while True:
+                response = requests.get(f"{full_url}{offset}", headers=self._get_headers())
+                response.raise_for_status()
+                data = response.json()
+                for txn in data['transactions']:
+                    transactions[account].append(txn)
+                if len(data['transactions']) < 500:
+                    break
+                offset += 500
+        return transactions
+    
 
 class MercurySource(Source):
     def __init__(self, log_status: Callable[[str], None], api_key: str, account_id: str, assets_account: str, **kwargs) -> None:
@@ -29,68 +74,61 @@ class MercurySource(Source):
         self.api_key = api_key
         self.account_id = account_id
         self.assets_account = assets_account
+        self.mercury_api = MercuryAPI(api_key)
+        self.downloaded_txns = self.mercury_api.fetch_mercury_transactions()
 
     @property
     def name(self) -> str:
         return 'mercury'
 
-    def fetch_mercury_transactions(self):
-        headers = {
-            'Authorization': f'Bearer {self.api_key}',
-            'Content-Type': 'application/json'
-        }
-        transactions = []
-        offset = 0
-        url = MERCURY_API_URL_TEMPLATE.format(account_id=self.account_id)
-
-        while True:
-            response = requests.get(f"{url}{offset}", headers=headers)
-            response.raise_for_status()
-            data = response.json()
-            transactions.extend(data['transactions'])
-            if len(data['transactions']) < 500:
-                break
-            offset += 500
-
-        return transactions
+   
 
     def create_beancount_transaction(self, txn: Dict[str, Any]) -> Transaction:
-        date = datetime.datetime.strptime(txn['date'], '%Y-%m-%d').date()
-        amount = D(txn['amount']['value'])
-        currency = txn['amount']['currency']
-        narration = txn['description']
-        payee = txn['merchant'] if txn['merchant'] else 'Unknown'
-
-        meta = {
-            'source': 'mercury',
-            'mercury_id': txn['id'],
-            'date': txn['date']
-        }
+        amount = D(txn['amount'])
+        narration = txn['bankDescription']
+        payee = txn['counterpartyNickname'] if txn['counterpartyNickname'] else txn['counterpartyName']
+        date = datetime.fromisoformat(txn['createdAt'].replace("Z", "+00:00"))
+        meta = collections.OrderedDict([
+            ('mercury_id', txn['id']),
+            ('date', date.date()),
+            ('kind', txn['kind']),
+            ('counterpartyName', txn['counterpartyName'])
+            ])
 
         postings = [
-            Posting(self.assets_account, Amount(amount, currency), None, None, None, None),
-            Posting(FIXME_ACCOUNT, Amount(-amount, currency), None, None, None, None)
+            Posting(self.assets_account, Amount(amount, 'USD'), None, None, None, meta),
+            Posting(FIXME_ACCOUNT, Amount(-amount, 'USD'), None, None, None, None)
         ]
 
-        return Transaction(meta, date, FLAG_OKAY, payee, narration, EMPTY_SET, EMPTY_SET, postings)
+        return Transaction(None, date.date(), FLAG_OKAY, payee, narration, EMPTY_SET, EMPTY_SET, postings)
 
     def prepare(self, journal: 'JournalEditor', results: SourceResults) -> None:
-        transactions = self.fetch_mercury_transactions()
-        beancount_entries = [self.create_beancount_transaction(txn) for txn in transactions]
+        for account, transactions in self.downloaded_txns.items():
+            if account != self.account_id:
+                continue
+            print(transactions)
+            beancount_entries = [self.create_beancount_transaction(txn) for txn in transactions]
 
-        for entry in beancount_entries:
-            results.add_pending_entry(ImportResult(date=entry.date, entries=[entry], info=None))
+            for entry in beancount_entries:
+                results.add_pending_entry(ImportResult(date=entry.date, entries=[entry], info=None))
 
-        results.add_account(self.assets_account)
+            results.add_account(self.assets_account)
 
     def is_posting_cleared(self, posting: Posting) -> bool:
-        return False
+        if not posting.meta:
+            return False
+        if 'mercury_id' in posting.meta:
+            return True
 
     def get_example_key_value_pairs(self, transaction: Transaction, posting: Posting) -> Dict[str, str]:
-        return {}
-
-    def get_associated_data(self, entry: Directive) -> Optional[List[AssociatedData]]:
-        return None
+        result = dict()
+        result['desc'] = transaction.narration
+        result['payee'] = transaction.payee
+        if 'kind' in posting.meta:
+            result['kind'] = posting.meta['kind']
+        if 'counterpartyName' in posting.meta:
+            result['counterpartyName'] = posting.meta['counterpartyName']
+        return result
 
 def load(spec: Dict[str, Any], log_status: Callable[[str], None]) -> Source:
     return MercurySource(log_status=log_status, **spec)
